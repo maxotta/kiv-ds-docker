1c306aa1727c35ce8e811e1fa7a2025c44c1683a
Max Otta <max.otta@gmail.com>
2021-09-13 11:28:14 +0200

    Added a possibility to delay service startup using the STARTUP_DELAY env. variable

--- kiv-ds-startup.sh
+++ kiv-ds-startup.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-LOGFILE=/tmp/kiv-ds-startup.log
-echo "BEGIN" >> $LOGFILE
-
+set -e
+LOG_TAG="<*> kiv-ds-startup: "
+echo "$LOG_TAG Entry point STARTING."
+echo "$LOG_TAG number of arguments: $#"
+echo "$LOG_TAG arguments: '$@'"
 # Start the OpenSSH daemon
-echo "Starting sshd." >> $LOGFILE
-/usr/sbin/sshd -D
 
+if [ $# -eq 0 ]; then
+    echo "$LOG_TAG Starting sshd (not detached)."
+    /usr/sbin/sshd -D
+else
+    echo "$LOG_TAG Starting sshd (detached daemon)."
+    /usr/sbin/sshd
+    if [ ! -z "$STARTUP_DELAY" ] ; then
+        echo "Startup delay is set to: $STARTUP_DELAY. Going to sleep for a while..."
+        sleep $STARTUP_DELAY
+    fi
+    exec "$@"
+fi
+# reached only if a service dies
+echo "$LOG_TAG Entry point TERMINATED."
 # EOF
